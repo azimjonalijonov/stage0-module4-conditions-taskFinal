b5e75554a9f6494b0d5e894225a405acf772a932
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-25 09:40:47 +0500

    ss

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import static java.lang.Math.abs;
+
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if(firstSide>abs(secondSide-thirdSide)&&firstSide<(secondSide+thirdSide)&&secondSide>abs(firstSide-thirdSide)&&secondSide<(firstSide+thirdSide)&&thirdSide>abs(secondSide-firstSide)&&thirdSide<(secondSide+firstSide)){
+            System.out.println("this is a valid triangle");
+        }else {
+            System.out.println("it's not a triangle");
+        }
     }
 }
